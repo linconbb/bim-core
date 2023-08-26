b52594b475e2d29ce4ac1536308b6ff6ca0c824d
linconbb <74726158+linconbb@users.noreply.github.com>
2023-08-26 16:24:25 +0800

    Update hyperspeed.sh

--- hyperspeed.sh
+++ hyperspeed.sh
@@ -23,10 +23,11 @@ check_bimc() {
 }
 
 print_info() {
-    echo "——————————————————————————— HyperSpeed —————————————————————————————"
-    echo "         bash <(wget -qO- https://bench.im/hyperspeed)"
+    echo "——————————————————————————— HyperSpeed 节点修复版—————————————————————————————"
+    echo "         bash <(curl -Lso- https://is.gd/Y7iQH0)"
+    echo "         项目修改自: https://bench.im/hyperspeed"
     echo "         项目修改自: https://github.com/zq/superspeed/"
-    echo "   脚本更新by ChinaNet AS4134  2023/8/18"
+    echo "         脚本更新by ChinaNet AS4134  2023/8/26"
     echo "————————————————————————————————————————————————————————————————————"
 }
 
